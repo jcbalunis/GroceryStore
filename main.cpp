8020fa3539df66ab4fc9a683c44d667adfcf2f83
jcbalunis <jbalunis@mail.csuchico.edu>
2018-02-21 17:21:43 -0800

    list ask

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Joseph Balunis
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,18 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+do
+{
+  
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+ }while( input != 'Q' && input != 'q' ); 
+
+
 
   return 0;
 }
