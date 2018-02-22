5a30706b5adbeff8bf3a777d1ebbcd031415c373
jcbalunis <jbalunis@mail.csuchico.edu>
2018-02-22 12:30:30 -0800

    done grocerylist

--- main.cpp
+++ main.cpp
@@ -3,23 +3,42 @@
 #include<string>
 using namespace std;
 
+
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-do
-{
-  
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
- }while( input != 'Q' && input != 'q' ); 
+        string list[5], food; //array of 5 strings
+        int numItems = 0;
+        char input;
+        do
+        {
+
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+               
+               
+               if( numItems >= 5)
+                {
+                cout<< "You'll need a bigger list. \n";
+                        input = 'q';
+                }
+               
+               
+               if( input == 'a' || input == 'A' )
+                {        
+                cout<< "What is the Item? \n";
+                cin>> food;
+                list[numItems] = food;
+                numItems ++;
+                }
+
+        }
+        while( input != 'Q' && input != 'q'); 
 
 
 
-  return 0;
+        return 0;
 }
